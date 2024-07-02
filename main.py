aefffa6afc895943fadf736de69ddc6aff5667a1
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-07-03 00:29:39 +0200

    Abandon ship, attempt audio synthesis from scratch!
    Currently have some sort of really nasty buzzing sound, I think it's an issue with mismatched data types or something, I'll sort that next time

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-import subprocess
 import time
+from collections.abc import Mapping
 
-subprocess.Popen([r'C:\Program Files\SuperCollider-3.13.0\scsynth.exe', '-t', '57120', '-l', '2'])  # noqa: S603
-time.sleep(10)
-print('\nRunning sclang...\n')
-subprocess.run([r'C:\Program Files\SuperCollider-3.13.0\sclang.exe', './startup.scd'], check=True)  # noqa: S603
+import numpy as np
+import pyaudio
+
+SAMPLE_RATE = 44100
+
+
+def audio_callback(
+    in_data: bytes | None,
+    frame_count: int,
+    time_info: Mapping[str, float],
+    status: int,
+) -> tuple[bytes | None, int]:
+
+    sine = np.sin(np.arange(0, 440 * frame_count, 440, dtype=np.float32))
+    return sine, pyaudio.paContinue
+
+
+if __name__ == '__main__':
+    session = pyaudio.PyAudio()
+    stream = session.open(
+        rate=SAMPLE_RATE,
+        channels=1,
+        format=pyaudio.paFloat32,
+        output=True,
+        frames_per_buffer=440,
+        stream_callback=audio_callback,
+    )
+    while stream.is_active():
+        time.sleep(0.1)
+    stream.close()
+    session.terminate()
