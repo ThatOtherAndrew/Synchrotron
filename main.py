1e9578db3fd57b590d5f472ad57f463ae97d275a
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-07-03 10:48:02 +0200

    Get a working sine wave, slight refactor

--- main.py
+++ main.py
@@ -7,28 +7,53 @@ import pyaudio
 SAMPLE_RATE = 44100
 
 
-def audio_callback(
-    in_data: bytes | None,
-    frame_count: int,
-    time_info: Mapping[str, float],
-    status: int,
-) -> tuple[bytes | None, int]:
+class Synchrotron:
+    def __init__(self):
+        self.sample_clock = 0
 
-    sine = np.sin(np.arange(0, 440 * frame_count, 440, dtype=np.float32))
-    return sine, pyaudio.paContinue
+    def get_buffer(self, length: int):
+        sine_window = np.linspace(
+            0,
+            2 * np.pi * 440 * SAMPLE_RATE / length,
+            num=length,
+            endpoint=False,
+            dtype=np.float32
+        )
+        return np.sin(sine_window)
+
+    def __pyaudio_callback(
+        self,
+        in_data: bytes | None,
+        frame_count: int,
+        time_info: Mapping[str, float],
+        status: int,
+    ) -> tuple[bytes | None, int]:
+        buffer = self.get_buffer(length=frame_count)
+        self.sample_clock += frame_count
+        return buffer, pyaudio.paContinue
+
+    def play(self) -> None:
+        pyaudio_session = pyaudio.PyAudio()
+        stream = pyaudio_session.open(
+            rate=SAMPLE_RATE,
+            channels=1,
+            format=pyaudio.paFloat32,
+            output=True,
+            frames_per_buffer=SAMPLE_RATE,
+            stream_callback=self.__pyaudio_callback,
+        )
+
+        try:
+            while stream.is_active():
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            print('Keyboard interrupt received')
+        finally:
+            print('Closing stream')
+            stream.close()
+            pyaudio_session.terminate()
 
 
 if __name__ == '__main__':
-    session = pyaudio.PyAudio()
-    stream = session.open(
-        rate=SAMPLE_RATE,
-        channels=1,
-        format=pyaudio.paFloat32,
-        output=True,
-        frames_per_buffer=440,
-        stream_callback=audio_callback,
-    )
-    while stream.is_active():
-        time.sleep(0.1)
-    stream.close()
-    session.terminate()
+    synchrotron = Synchrotron()
+    synchrotron.play()
