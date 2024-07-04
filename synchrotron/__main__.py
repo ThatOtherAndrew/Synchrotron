83b90da9d399cb59c89a34e98bb0d2097ea804f7
ThatOtherAndrew <stroev.andrew@gmail.com>
2024-07-04 19:24:00 +0200

    (WIP) Refactor, yet again, to make node graph processing "downstream" instead of "upstream"

--- synchrotron/__main__.py
+++ synchrotron/__main__.py
@@ -1,42 +1,61 @@
-import time
+from queue import SimpleQueue
 
 import pyaudio
+from graphlib import TopologicalSorter
 
-from . import nodes
+from .nodes import Input, Node, Output
 
 
 class Synchrotron:
-    def __init__(self, sample_rate: int = 44100):
+    def __init__(self, sample_rate: int = 44100, buffer_size: int = 44100):
         self.pyaudio_session = pyaudio.PyAudio()
         self.global_clock = 0
+        self.nodes: dict[Node, set[Node]] = {}
+        self.connections: dict[Output, set[Input]] = {}
+        self.pending_connections: SimpleQueue[tuple[Output, Input, bool]] = SimpleQueue()
 
-        # It'd be cool to have a dynamic sample rate, but it would be such an implementation headache
+        # It'd be cool to have a dynamic sample rate and buffer size, but it would be such an implementation headache
         self.sample_rate = sample_rate
+        self.buffer_size = buffer_size
 
-    def play(self, playback_node: nodes.audio.PlaybackNode) -> None:
-        playback_node.start_streaming()
-        try:
-            while playback_node.stream.is_active():
-                time.sleep(0.1)
-        except KeyboardInterrupt:
-            print('Keyboard interrupt received')
-        finally:
-            print('Closing stream')
-            playback_node.stream.close()
-            self.pyaudio_session.terminate()
+    def add_node(self, node: Node):
+        if node in self.nodes:
+            return
+
+        self.nodes[node] = set()
+        for output in node.outputs.values():
+            self.connections[output] = set()
+
+    def connect(self, from_output: Output, to_input: Input):
+        self.pending_connections.put((from_output, to_input, True))
+
+    def disconnect(self, from_output: Output, to_input: Input):
+        self.pending_connections.put((from_output, to_input, False))
+
+    def tick(self):
+        while not self.pending_connections.empty():
+            source, destination, connect = self.pending_connections.get()
+            if connect:
+                self.nodes[source.node].add(destination.node)
+                self.connections[source].add(destination)
+            else:
+                self.connections[source].remove(destination)
+                if not any(
+                    self.connections[output].intersection(destination.node.inputs.values())
+                    for output in source.node.outputs.values()
+                ):
+                    self.nodes[source.node].remove(destination.node)
+
+        node_graph = TopologicalSorter(self.nodes)
+        node_graph.prepare()
+        while node_graph.is_active():
+            for node in node_graph.get_ready():
+                node.render()
+                for output in node.outputs.values():
+                    for target_input in self.connections[output]:
+                        target_input.buffer = output.buffer
+                node_graph.done(node)
 
 
 if __name__ == '__main__':
-    synchrotron = Synchrotron()
-    output = nodes.audio.PlaybackNode(synchrotron)
-    freq_l = nodes.data.ConstantNode(440.)
-    sine_l = nodes.audio.SineNode()
-    freq_r = nodes.data.ConstantNode(660.)
-    sine_r = nodes.audio.SineNode()
-
-    output.inputs['left'].link(sine_l.outputs['sine'])
-    sine_l.inputs['frequency'].link(freq_l.outputs['value'])
-    output.inputs['right'].link(sine_r.outputs['sine'])
-    sine_r.inputs['frequency'].link(freq_r.outputs['value'])
-
-    synchrotron.play(output)
+    ...
--- synchrotron/nodes/base.py
+++ synchrotron/nodes/base.py
@@ -6,29 +6,13 @@ from typing import Any, Callable
 class Input:
     def __init__(self, node: Node) -> None:
         self.node = node
-        self.source = None
-
-    def link(self, source: Output) -> None:
-        self.source = source
-
-    def read(self, offset: int, duration: int) -> Any:
-        if self.source is None:
-            raise RuntimeError('input cannot be read from as it has no source')
-
-        return self.source.render(offset, duration)
+        self.buffer = None
 
 
 class Output:
-    def __init__(self, node: Node, func: Callable[[int, int], Any]) -> None:
+    def __init__(self, node: Node) -> None:
         self.node = node
-        self._render_func = func
-        self.render_cache: tuple[tuple[int, int], Any] = (0, 0), None
-
-    def render(self, offset: int, duration: int) -> Any:
-        if self.render_cache[0] != (offset, duration) or self.render_cache[1] is None:
-            self.render_cache = (offset, duration), self._render_func(offset, duration)
-
-        return self.render_cache[1]
+        self.buffer = None
 
 
 class Node:
